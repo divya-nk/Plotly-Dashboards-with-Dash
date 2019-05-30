eb73e56fe168bab54e1f8ac1a6c326e77eb381ad
divya-nk <mnavane@g.clemson.edu>
2019-05-30 16:53:55 -0400

    scatter

---
+++ 1-01-PlotlyBasics/dbasics.py
@@ -0,0 +1,24 @@
+import numpy as np
+import plotly.offline as pyo
+import plotly.graph_objs as go
+
+np.random.seed(42)
+random_x = np.random.randint(1,101,100)
+random_y = np.random.randint(1,101,100)
+
+data = [go.Scatter(x=random_x,
+                    y= random_y,
+                    mode ='markers',
+                    marker=dict(
+                            size=12,
+                            color='rgb(51,204,153)',
+                            symbol='pentagon',
+                            line=dict(width=2)
+                    ))]
+layout = go.Layout(title='Scatter Plot',
+                    xaxis={'title': 'My x-axis'},
+                    yaxis=dict(title='My y-axis'),
+                    hovermode='closest')
+
+fig = go.Figure(data=data, layout=layout)
+pyo.plot(fig, filename='scatter.html')
